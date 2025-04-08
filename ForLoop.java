d8d77d935244d443dafad78aeed1367f1a9c9917
arifinlenna <arifin@lenna.ai>
2025-04-08 15:02:34 +0700

    java-todo-list new

--- ForLoop.java
+++
@@ -1,7 +0,0 @@
-public class ForLoop {
-    public static void main(String[] args) {
-        for (var counter = 1; counter <= 10; counter++) {
-            System.out.println("Perulangan " + counter);
-        }
-    }
-}
--- TipeDataString.java
+++
@@ -1,8 +0,0 @@
-public class TipeDataString {
-    public static void main(String[] args) {
-        String firstname = "Nur";
-        String lastname = "Arifin";
-
-        System.out.println(firstname + " " + lastname);
-    }
-}
