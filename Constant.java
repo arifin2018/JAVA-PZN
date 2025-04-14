ec69e0c6f6fc6f5234895a1cba31977d47ef6df0
arifinlenna <arifin@lenna.ai>
2025-04-14 09:36:15 +0700

    constant

---
+++ Constant.java
@@ -0,0 +1,5 @@
+public class Constant {
+    public static String APPLICATION = "belajar java";
+    public static int VERSION = 1;
+    
+}
---
+++ karyawan/Utill/MathUtil.java
@@ -0,0 +1,11 @@
+package karyawan.Utill;
+
+public class MathUtil {
+    public static int sum(int... values){
+        int total = 0;
+        for (int i : values) {
+            total += i;
+        }
+        return total;
+    }
+}
