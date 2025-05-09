79f137fdb45cc921f0beb3e1f1391cdbf2c7617a
arifinlenna <arifin@lenna.ai>
2025-05-09 08:36:53 +0700

    first commit database

--- StringBuffer.java
+++
@@ -1,9 +0,0 @@
-public class StringBuffer {
-    public static void main(String[] args) {
-        StringBuilder string = new StringBuilder();
-        string.append("nur");
-        string.append(" ");
-        string.append("arfin");
-        System.out.println(string);
-    }
-}
